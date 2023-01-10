5165dd27bfae117d7e498612f20b72e9238a8278
chaimPaneth <caapaneth@gmail.com>
2023-01-10 18:39:20 +0200

    config

--- src/data/config.js
+++ src/data/config.js
@@ -8,5 +8,8 @@ export const AUTH_DOMAIN = IS_DEV
 export const AUTH_ID = IS_DEV
   ? "l2EdgB8cAh3swlNTBO476nqUlfzJvg2W"
   : "ioDLFKxzfv1TprtHgwB0lZy4Vy5pQlN1";
-  
+
 export const POLOTNO_KEY = "FA29LdEvOAJdMenXqqEy"
+
+export const API = "http://35.172.79.130:4000/graphql"
+export const TOKEN = "c819f484-71e7-4514-b5ab-98d980f48442"

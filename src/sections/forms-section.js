6a9ef486916a9f3e3f7ab11f4d52ecd41dc8e92d
chaimPaneth <caapaneth@gmail.com>
2024-09-17 15:46:27 +0300

    fix

--- src/sections/forms-section.js
+++ src/sections/forms-section.js
@@ -7,6 +7,7 @@ import {
   Spinner,
   Switch,
   NumericInput,
+  Divider,
 } from "@blueprintjs/core";
 import FaPlus from "@meronex/icons/fa/FaPlus";
 import FaList from "@meronex/icons/fa/FaList";
@@ -14,6 +15,7 @@ import FaTrash from "@meronex/icons/fa/FaTrash";
 import FaSave from "@meronex/icons/fa/FaSave";
 import { SectionTab } from "polotno/side-panel";
 import { useForms } from "../data/graphql/api";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const fieldStyle = { marginBottom: 10 };
 
@@ -22,10 +24,11 @@ const FormCard = ({ form, onUpdate, onDelete }) => {
   const [subTitle, setSubTitle] = useState(form.subTitle || "");
   const [hasImage, setHasImage] = useState(form.hasImage || false);
   const [fields, setFields] = useState(
-    form.fields.map((field) => ({
+    form.fields.map((field, index) => ({
       ...field,
-      isNew: false,  // Track if it's a new field
-      isDeleted: false,  // Track if it's marked for deletion
+      isNew: false, // Track if it's a new field
+      isDeleted: false, // Track if it's marked for deletion
+      index, // Add index to each field
     })) || []
   );
 
@@ -38,7 +41,15 @@ const FormCard = ({ form, onUpdate, onDelete }) => {
   const addNewField = () => {
     setFields([
       ...fields,
-      { title: "", placeholder: "", max: 255, key: "", required: false, isNew: true },
+      {
+        title: "",
+        placeholder: "",
+        max: 255,
+        key: "",
+        required: false,
+        isNew: true,
+        index: fields.length,
+      },
     ]);
   };
 
@@ -54,17 +65,18 @@ const FormCard = ({ form, onUpdate, onDelete }) => {
 
   const updateInput = () => {
     const createFields = fields
-      .filter((field) => field.isNew && !field.isDeleted)  // New fields to be created
+      .filter((field) => field.isNew && !field.isDeleted)
       .map((field) => ({
         title: field.title,
         placeholder: field.placeholder,
         max: field.max,
         key: field.key,
         required: field.required,
+        index: field.index,
       }));
 
     const updateFields = fields
-      .filter((field) => !field.isNew && !field.isDeleted)  // Existing fields to be updated
+      .filter((field) => !field.isNew && !field.isDeleted)
       .map((field) => ({
         where: { id: field.id },
         data: {
@@ -73,8 +85,13 @@ const FormCard = ({ form, onUpdate, onDelete }) => {
           max: { set: field.max },
           key: { set: field.key },
           required: { set: field.required },
+          index: { set: field.index },
         },
       }));
+
+    const deleteFields = fields
+      .filter((field) => field.isDeleted)
+      .map((field) => ({ id: field.id }));
 
     return {
       title: { set: name },
@@ -83,6 +100,7 @@ const FormCard = ({ form, onUpdate, onDelete }) => {
       fields: {
         create: createFields,
         update: updateFields,
+        delete: deleteFields,
       },
     };
   };
@@ -96,8 +114,23 @@ const FormCard = ({ form, onUpdate, onDelete }) => {
     );
   }, [name, subTitle, hasImage, fields, form]);
 
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+
+    // If dropped outside the list, do nothing
+    if (!destination) return;
+
+    // Reorder fields based on drag-and-drop
+    const reorderedFields = Array.from(fields);
+    const [removed] = reorderedFields.splice(source.index, 1);
+    reorderedFields.splice(destination.index, 0, removed);
+
+    // Update indexes
+    setFields(reorderedFields.map((field, index) => ({ ...field, index })));
+  };
+
   return (
-    <Card style={{ padding: 10, marginBottom: 30 }} key={form.id}>
+    <Card style={{ padding: 10, marginBottom: 30 }} key={form.id} elevation={3}>
       <div style={{ marginBottom: 10 }}>
         <InputGroup
           style={fieldStyle}
@@ -119,71 +152,131 @@ const FormCard = ({ form, onUpdate, onDelete }) => {
           label="Has Image"
         />
         <h4>Form Fields</h4>
-        {fields
-          .filter((field) => !field.isDeleted)  // Only show non-deleted fields
-          .map((field, index) => (
-            <Card key={index} style={{ padding: "10px", marginBottom: "10px" }}>
-              <InputGroup
-                style={fieldStyle}
-                value={field.title}
-                onChange={(e) =>
-                  handleFieldChange(index, { title: e.target.value })
-                }
-                placeholder="Field Title"
-                required
-              />
-              <InputGroup
-                style={fieldStyle}
-                value={field.placeholder}
-                onChange={(e) =>
-                  handleFieldChange(index, { placeholder: e.target.value })
-                }
-                placeholder="Field Placeholder"
-              />
-              <NumericInput
-                style={fieldStyle}
-                value={field.max}
-                onValueChange={(value) =>
-                  handleFieldChange(index, { max: value })
-                }
-                placeholder="Max Characters"
-              />
-              <InputGroup
-                style={fieldStyle}
-                value={field.key}
-                onChange={(e) =>
-                  handleFieldChange(index, { key: e.target.value })
-                }
-                placeholder="Field Key"
-              />
-              <Switch
-                style={fieldStyle}
-                checked={field.required}
-                onChange={(e) =>
-                  handleFieldChange(index, { required: e.target.checked })
-                }
-                label="Required Field"
-              />
-              <Button
-                icon={<FaTrash />}
-                intent="danger"
-                onClick={() => deleteField(index)}
-              >
-                Delete Field
-              </Button>
-            </Card>
-          ))}
 
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="fields">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {fields
+                  .filter((field) => !field.isDeleted) // Only show non-deleted fields
+                  .map((field, index) => (
+                    <Draggable
+                      key={index}
+                      draggableId={index.toString()}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <p>Field {index + 1}</p>
+                          <Card
+                            style={{ padding: "10px", marginBottom: "10px" }}
+                            elevation={1}
+                          >
+                            <InputGroup
+                              style={fieldStyle}
+                              value={field.title}
+                              onChange={(e) =>
+                                handleFieldChange(index, {
+                                  title: e.target.value,
+                                })
+                              }
+                              placeholder="Field Title"
+                              required
+                            />
+                            <InputGroup
+                              style={fieldStyle}
+                              value={field.placeholder}
+                              onChange={(e) =>
+                                handleFieldChange(index, {
+                                  placeholder: e.target.value,
+                                })
+                              }
+                              placeholder="Field Placeholder"
+                            />
+                            <NumericInput
+                              style={fieldStyle}
+                              value={field.max}
+                              onValueChange={(value) =>
+                                handleFieldChange(index, { max: value })
+                              }
+                              placeholder="Max Characters"
+                            />
+                            <InputGroup
+                              style={fieldStyle}
+                              value={field.key}
+                              onChange={(e) =>
+                                handleFieldChange(index, {
+                                  key: e.target.value,
+                                })
+                              }
+                              placeholder="Field Key"
+                            />
+                            <Switch
+                              style={fieldStyle}
+                              checked={field.required}
+                              onChange={(e) =>
+                                handleFieldChange(index, {
+                                  required: e.target.checked,
+                                })
+                              }
+                              label="Required Field"
+                            />
+                            <Button
+                              icon={<FaTrash />}
+                              intent="danger"
+                              onClick={() => deleteField(index)}
+                            >
+                              Delete Field
+                            </Button>
+                          </Card>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: 20,
+          }}
+        >
           <Button icon={<FaPlus />} onClick={addNewField}>
             Add New Field
           </Button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            paddingTop: 20,
+            borderTopColor: "lightgray",
+            borderTopWidth: 1,
+            borderTopStyle: "solid",
+          }}
+        >
           <Button
             icon={<FaSave />}
             onClick={() => onUpdate(form.id, updateInput())}
             disabled={!unsavedChanges}
           >
             Save Form
+          </Button>
+          <Button
+            icon={<FaTrash />}
+            intent="danger"
+            onClick={() => onDelete(form.id)}
+          >
+            Delete Form
           </Button>
         </div>
       </div>
